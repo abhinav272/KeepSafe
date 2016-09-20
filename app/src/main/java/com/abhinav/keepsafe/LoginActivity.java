28bb9dee87f4725f9f32fb39d5e3cbcfe1ea5186
Abhinav Sharma <abhinav.ih@gmail.com>
2016-09-20 13:48:26 +0530

    login feature made functional

--- app/src/main/java/com/abhinav/keepsafe/LoginActivity.java
+++ app/src/main/java/com/abhinav/keepsafe/LoginActivity.java
@@ -1,10 +1,16 @@
 package com.abhinav.keepsafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.abhinav.keepsafe.Utils.KeepSafeKeys;
+import com.abhinav.keepsafe.Utils.KeepSafePrefs;
 import com.andrognito.pinlockview.IndicatorDots;
 import com.andrognito.pinlockview.PinLockListener;
 import com.andrognito.pinlockview.PinLockView;
@@ -16,15 +22,21 @@ public class LoginActivity extends AppCompatActivity {
 
     private PinLockView mPinLockView;
     private IndicatorDots mIndicatorDots;
+    private TextView tvBanner;
+    private String pin1, pin2;
+    private boolean flag;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        setupUI();
+        setupLockView();
+        if (flag = (getIntent().getBooleanExtra(KeepSafeKeys.IS_USER_PIN_AVAILABLE, false)))
+            setupUI();
+        else setupPinUI();
     }
 
-    private void setupUI() {
+    private void setupLockView() {
         mPinLockView = (PinLockView) findViewById(R.id.pin_lock_view);
         mIndicatorDots = (IndicatorDots) findViewById(R.id.indicator_dots);
         if (mIndicatorDots != null) {
@@ -36,17 +48,50 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onComplete(String pin) {
                 Log.e("onComplete: ", pin);
+                if(!flag){
+                    if (pin1 == null) {
+                        pin1 = pin;
+                        tvBanner.setText(R.string.confirm_pin_string);
+                        mPinLockView.resetPinLockView();
+                    } else{
+                        pin2 = pin;
+                        if(pin1.equals(pin2)){
+                            KeepSafePrefs.getInstance().setKeepSafeUserPin(LoginActivity.this, pin1);
+                            Toast.makeText(LoginActivity.this, "PIN saved", Toast.LENGTH_SHORT).show();
+                            navigateToHome();
+                        }
+                    }
+                } else {
+                    if(pin.equals(KeepSafePrefs.getInstance().getKeepSafeUserPin(LoginActivity.this)))
+                        navigateToHome();
+                }
             }
 
             @Override
             public void onEmpty() {
-                Log.e("onEmpty: ","");
+                Log.e("onEmpty: ", "");
             }
 
             @Override
             public void onPinChange(int pinLength, String intermediatePin) {
-                Log.e("onPinChange: ", intermediatePin+"  "+pinLength);
+                Log.e("onPinChange: ", intermediatePin + "  " + pinLength);
             }
         });
+    }
+
+    private void navigateToHome() {
+        // add transition to navigate to Home Activity
+        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+        finish();
+    }
+
+    private void setupPinUI() {
+        tvBanner = (TextView) findViewById(R.id.tv_banner);
+        tvBanner.setVisibility(View.VISIBLE);
+        tvBanner.setText(getString(R.string.choose_pin_string));
+    }
+
+    private void setupUI() {
+        // left blank for future changes
     }
 }
