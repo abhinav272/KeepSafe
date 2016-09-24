396e3167bef97cef44780a9ee92a4577e27f064b
Abhinav Sharma <abhinav.ih@gmail.com>
2016-09-24 19:16:47 +0530

    Partial changes for Mascot

--- app/src/main/java/com/abhinav/keepsafe/fragments/KSListFragment.java
+++ app/src/main/java/com/abhinav/keepsafe/fragments/KSListFragment.java
@@ -1,19 +1,26 @@
 package com.abhinav.keepsafe.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.abhinav.keepsafe.HomeActivity;
 import com.abhinav.keepsafe.R;
 import com.abhinav.keepsafe.Utils.AccountModel;
 import com.abhinav.keepsafe.Utils.KSDatabaseHelper;
 import com.abhinav.keepsafe.adapters.KSAdapter;
+import com.abhinav.keepsafe.services.MascotService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,7 +40,20 @@ public class KSListFragment extends BaseFragment {
         View view;
         view = inflater.inflate(R.layout.fragment_ks_list, container, false);
         setupUI(view);
+        setHasOptionsMenu(true);
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.safe_item_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        getActivity().startService(new Intent(getActivity(), MascotService.class));
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -51,6 +71,9 @@ public class KSListFragment extends BaseFragment {
         ksAdapter = new KSAdapter(getActivity(), accountModels);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(ksAdapter);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.app_bar);
+        ((HomeActivity) getActivity()).setupSupportActionBar(toolbar);
     }
 
     public class AllItemsFetcherTask extends AsyncTask<Void, Void, List<AccountModel>>{
